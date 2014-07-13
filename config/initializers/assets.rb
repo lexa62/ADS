32d19867c15c60114da2ea4a262836b9816366b9
lexa62 <lexa62@tut.by>
2014-07-13 13:55:19 +0300

    Asset Pipeline for web-app-theme - close #6

---
+++ config/initializers/assets.rb
@@ -0,0 +1,4 @@
+Rails.application.config.assets.precompile += %w( nested_form.js )
+Rails.application.config.assets.precompile += %w( web-app-theme/base.css )
+Rails.application.config.assets.precompile += %w( web-app-theme/themes/default/style.css )
+Rails.application.config.assets.precompile += %w( web-app-theme/override.css )
