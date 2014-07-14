c3790f677f1e01bc8aa02ae4767813529c8f75e8
lexa62 <lexa62@tut.by>
2014-07-15 00:11:36 +0300

    use right dependent option for ad_type - close #12

--- app/models/ad_type.rb
+++ app/models/ad_type.rb
@@ -1,8 +1,6 @@
 class AdType < ActiveRecord::Base
-  before_destroy :can_delete?
-
   validates :name, presence: true, length: { maximum: 15 }
-  has_many :ads, inverse_of: :ad_type, dependent: :destroy
+  has_many :ads, inverse_of: :ad_type, dependent: :restrict_with_error
 
   def can_delete?
     ads.empty?
